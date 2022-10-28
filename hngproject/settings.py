93cae5213ccea0d55e91abe7bca6ff1cf1e10789
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-10-28 18:40:09 +0100

    Further work done for deployment

--- hngproject/settings.py
+++ hngproject/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mta$gx-^5_3&^ni1zl#b==aqa7l@$-nr@hc#+gtl!m0=chig7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    'collins-iloabuchi-hngi9.herokuapp.com/'
+    'www.collins-iloabuchi-hngi9.herokuapp.com/'
 ]
 
 
