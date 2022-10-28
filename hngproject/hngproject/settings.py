e18c9b04e05b89db1e986b5615bebf0396bedd85
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-10-28 12:22:00 +0100

    Further work done for deployment

--- hngproject/hngproject/settings.py
+++ hngproject/hngproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mta$gx-^5_3&^ni1zl#b==aqa7l@$-nr@hc#+gtl!m0=chig7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
