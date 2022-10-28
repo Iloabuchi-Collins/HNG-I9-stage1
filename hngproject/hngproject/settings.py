167829c0b82bb33e2a82967a1d179de06c1f9b02
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-10-28 21:13:28 +0100

    Further work done for deployment

--- hngproject/hngproject/settings.py
+++ hngproject/hngproject/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
