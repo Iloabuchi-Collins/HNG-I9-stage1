acd0eeabaa2dc3ab9ca976d7cb270634f70b5467
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-10-28 22:30:28 +0100

    Further work done for deployment

--- hngproject/hngproject/wsgi.py
+++ hngproject/hngproject/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
@@ -16,3 +17,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hngproject.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'project_name'))
+sys.path.insert(0, root_path)
