f9741324f5271e316f12b22a5e1f5aee5953ddeb
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-10-29 06:09:11 +0100

    Further work done for deployment

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import SlackUser
 from .serializers import SlackUserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 
 #Create your views here.
@@ -11,3 +14,7 @@ class SlackUserList(generics.ListCreateAPIView):
 class SlackUserDetail(generics.RetrieveAPIView):
 	queryset = SlackUser.objects.all()
 	serializer_class = SlackUserSerializer 
+
+class JsonView(APIView):
+    def get(self, request):
+        return Response({'some': 'data'})
